086c3e11b482d830d8e6db457c49a6ed0c468dd7
Peter W. Draper <p.w.draper@durham.ac.uk>
2009-02-25 12:23:02 +0000

    Improve msgOutifv changes so that routine compiles and behaves as before

--- adamprint.c
+++ adamprint.c
@@ -16,10 +16,26 @@ void adamprint( FILE *file, char *fmt, ... )
 {
     va_list args;
     int status = SAI__OK;
+    char *fmtcpy;
+    int i;
+    int j;
+    int len;
+
+    /*  Strip out any 4 character escape sequences from format string.
+     *  These are added by SExtractor and shouldn't be passed through the 
+     *  message system. */
+    len = strlen( fmt );
+    fmtcpy = (char *) malloc( (size_t) len + 1 );
+    for ( i = 0, j = 0; i < len; i++, j++ ) {
+        if ( fmt[i] == '\33' ) i += 4;
+        fmtcpy[j] = fmt[i];
+    }
+    fmtcpy[j]= '\0';
 
     /* Write out the string via the message system */
     va_start( args, fmt );
-    msgOutifv( MSG__QUIET, " ", string, args, &status );
+    msgOutifv( MSG__QUIET, " ", fmtcpy, args, &status );
     va_end( args );
+    free( fmtcpy );
 
 }
